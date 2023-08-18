8cf0522cb9590126a7835f2f3cd8f30a8a45f2d0
J3ffn <jefferson.mangueira12@gmail.com>
2023-08-18 11:18:41 -0300

    add: callback function to sendMessage function

---
+++ str-producer/src/main/java/io/j3ffn/strproducer/services/StringProducerService.java
@@ -0,0 +1,29 @@
+package io.j3ffn.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message).addCallback(
+                sucess -> {
+                    if (sucess != null) {
+                        log.info("Enviada com sucesso! {}", message);
+                        log.info("Partition {}, Offset {}",
+                                sucess.getRecordMetadata().partition(),
+                                sucess.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.info("Não foi possível enviar {}", message)
+        );
+    }
+
+}
